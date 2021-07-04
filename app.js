50ba6f910dd054bdbc22b8f351764a17dfbb9906
unknown <kubawojewodzki46@gmail.com>
2021-07-04 12:23:38 +0200

    navbar fix

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const navSlide = () => {
             if(link.style.animation){
                   link.style.animation=''}
             else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/4+0.8}s`
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/4+0.2}s`
             }
     })
    
